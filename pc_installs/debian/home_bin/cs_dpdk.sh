f090e32eda8a7c0c401086592e49a05cccc3ddfe
IgorLe <igorle@mallanox.com>
2017-06-12 16:26:14 +0300

    add script for dpdk tags to home bin

---
+++ pc_installs/debian/home_bin/cs_dpdk.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# http://cscope.sourceforge.net/large_projects.html
+
+PWD=`pwd`
+
+##	-path "$LNX/arch/*" ! -path "$LNX/arch/i386*" -prune -o               \
+##	-path "$LNX/include/asm-*" ! -path "$LNX/include/asm-i386*" -prune -o \
+##	-path "$LNX/tmp*" -prune -o                                           \
+##	-path "$LNX/Documentation*" -prune -o                                 \
+##	-path "$LNX/scripts*" -prune -o                                       \
+##	-path "$LNX/drivers*" -prune -o                                       \
+##      -name "*.[chxsS]" -print >/home/jru/cscope/cscope.files
+
+#find "$PWD/apps/pcie" "$PWD/dpe" "$PWD/samples/pci_stack/pci_stack_app/src" -name "*.[chxsS]" -print > cscope.files
+#find "$PWD/dpe" "$PWD/samples/pci_stack/pci_stack_app/src" -name "*.[chxsS]" -print > cscope.files
+#find "/home/npsdb/br/ldk/x86_64/linux" "$PWD/ldk/build/nps_transp" -name "*.[chxsS]" -print > cscope.files
+#find "$PWD/dpe" "$PWD/apps/pci_tr" -name "*.[chxsS]" -print > cscope.files
+
+find . "/usr/include" -path "*/build" -prune -o -name "*.[chxsS]" -print > cscope.files
+cscope -b
+ctags -R --exclude="build"
